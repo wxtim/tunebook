6f3a1926d2dfba3dd6eb940c6e0f1019a90a5a3a
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2021-08-03 09:10:46 +0100

    added pdf version

--- source/conf.py
+++ source/conf.py
@@ -27,7 +27,7 @@ author = 'Tim Pillinger'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['abcm2svg']
+extensions = ['abcm2svg', 'sphinx.ext.imgconverter']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
